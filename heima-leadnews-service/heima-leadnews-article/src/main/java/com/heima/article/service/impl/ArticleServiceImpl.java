bfc21e7ac37d33b000edddbca21f77f07110de63
chengr25 <384372602@qq.com>
2024-07-23 21:34:56 +0800

    feature:自媒体作者端提交文章后，在用户app端生成html链接

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/impl/ArticleServiceImpl.java
@@ -1,6 +1,7 @@
 package com.heima.article.service.impl;
 
 import com.alibaba.cloud.commons.lang.StringUtils;
+import com.alibaba.fastjson.JSONArray;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.heima.article.mapper.ArticleConfigMapper;
@@ -8,6 +9,8 @@ import com.heima.article.mapper.ArticleContentMapper;
 import com.heima.article.mapper.ArticleMapper;
 import com.heima.article.service.ArticleService;
 import com.heima.common.constants.ArticleLoadingModeConstant;
+import com.heima.common.exception.CustomException;
+import com.heima.file.service.FileStorageService;
 import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
 import com.heima.model.article.pojos.ApArticle;
@@ -15,12 +18,20 @@ import com.heima.model.article.pojos.ApArticleConfig;
 import com.heima.model.article.pojos.ApArticleContent;
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.common.enums.AppHttpCodeEnum;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, ApArticle> implements ArticleService{
@@ -33,6 +44,15 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, ApArticle> im
 
     @Autowired
     ArticleContentMapper articleContentMapper;
+
+    @Autowired
+    FileStorageService fileStorageService;
+
+    @Autowired
+    Configuration configuration;
+
+    @Autowired
+    ArticleServiceImpl proxy;
 
     // 单页最大加载的数字
     private final static short MAX_PAGE_SIZE = 50;
@@ -99,6 +119,35 @@ public class ArticleServiceImpl extends ServiceImpl<ArticleMapper, ApArticle> im
             apArticleContent.setContent(dto.getContent());
             articleContentMapper.updateById(apArticleContent);
         }
+        proxy.buildArticleToMinIO(apArticle,dto.getContent());
         return ResponseResult.okResult(apArticle.getId());
+    }
+
+    @Async
+    void buildArticleToMinIO(ApArticle apArticle, String content) {
+        //4.1 获取文章内容
+        if(StringUtils.isBlank(content)){
+            throw new CustomException(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        //4.2 文章内容通过freemarker生成html文件
+        Template template = null;
+        StringWriter out = new StringWriter();
+        try {
+            template = configuration.getTemplate("article.ftl");
+            //数据模型
+            Map<String, Object> contentDataModel = new HashMap();
+            contentDataModel.put("content", JSONArray.parseArray(content));
+            //合成
+            template.process(contentDataModel, out);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        //4.3 把html文件上传到minio中
+        InputStream in = new ByteArrayInputStream(out.toString().getBytes());
+        String path = fileStorageService.uploadHtmlFile("", apArticle.getId() + ".html", in);
+
+        //4.4 修改ap_article表，保存static_url字段
+        update(Wrappers.<ApArticle>lambdaUpdate().eq(ApArticle::getId, apArticle.getId())
+                .set(ApArticle::getStaticUrl, path));
     }
 }
